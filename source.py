86357f35ec693896c1ee687900977fda19dc4c39
Skylar Clark <91299080+6306@users.noreply.github.com>
2023-04-16 17:36:48 -0500

    4.16.2023
    Fixed a bunch of shit that threw error messages.

--- source.py
+++ source.py
@@ -1,66 +1,66 @@
-import cv2
 import tweepy
-import requests
+import cv2
 import os
+import time
 
-# Set up your Twitter API credentials
-consumer_key = "YOUR_CONSUMER_KEY"
-consumer_secret = "YOUR_CONSUMER_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+# Twitter API credentials
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
 
-# Authenticate with Twitter API
+# Authenticate with Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-# Create the Tweepy API object
 api = tweepy.API(auth)
 
-# Define the path to your video file
-video_file = "PATH_TO_VIDEO_FILE"
+# Open the video file
+video = cv2.VideoCapture("video.mp4")
 
-# Create a directory to store the extracted frames
-frames_folder = "PATH_TO_FRAMES_FOLDER"
+# Get the framerate of the video
+fps = video.get(cv2.CAP_PROP_FPS)
+
+# If the framerate is zero, try specifying it manually
+if fps == 0:
+    fps = 25  # Replace 25 with the actual framerate of the video
+
+# Calculate the duration of the video
+frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+duration = frame_count / fps
+
+# Create a folder to store the frames
+frames_folder = "frames"
 if not os.path.exists(frames_folder):
     os.makedirs(frames_folder)
 
-# Open the video file and get its properties
-cap = cv2.VideoCapture(video_file)
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-duration = frame_count / fps
+# Loop through the frames
+for i in range(frame_count):
+    # Set the current frame position
+    video.set(cv2.CAP_PROP_POS_FRAMES, i)
 
-# Set the frame interval (1 frame per second)
-frame_interval = 5 * int(fps)
+    # Read the next frame from the video
+    ret, frame = video.read()
 
-# Set the starting frame number to 0
-frame_num = 0
+    # If the frame was successfully read
+    if ret:
+        # Save the frame to a file
+        filename = os.path.join(frames_folder, f"frame{i:06d}.jpg")
+        cv2.imwrite(filename, frame)
 
-# Loop through the video frames and extract every nth frame
-while cap.isOpened():
-    ret, frame = cap.read()
+        # If this is the 5th frame, post it to Twitter
+        if (i + 1) % 5 == 0:
+            # Wait for 30 minutes before posting the frames
+            time.sleep(1800)
 
-    if not ret:
-        break
+            # Create a tweet with the frames
+            tweet = "Check out these frames from my video!"
+            media = [api.media_upload(os.path.join(frames_folder, f"frame{j:06d}.jpg")) for j in range(i - 4, i + 1)]
+            api.update_status(tweet, media_ids=[media_item.media_id for media_item in media])
 
-    if frame_num % frame_interval == 0:
-        # Save the extracted frame to the frames folder
-        frame_filename = f"frame_{frame_num}.jpg"
-        cv2.imwrite(os.path.join(frames_folder, frame_filename), frame)
+# Release the video capture object
+video.release()
 
-        # Upload the frame to Twitter
-        media = api.media_upload(os.path.join(frames_folder, frame_filename))
-
-        # Post the frame to Twitter with a caption
-        caption = "Enter whatever you want to say with your frames."
-        api.update_status(status=caption, media_ids=[media.media_id])
-
-    frame_num += 1
-
-    # Stop the loop if we have processed all the frames
-    if frame_num == frame_count:
-        break
-
-# Release the video file and print some statistics
-cap.release()
-print(f"Extracted {frame_num} frames from the video ({duration:.2f} seconds, {fps:.2f} FPS).")
+# Remove the frames folder and all its contents
+for file in os.listdir(frames_folder):
+    os.remove(os.path.join(frames_folder, file))
+os.rmdir(frames_folder)
